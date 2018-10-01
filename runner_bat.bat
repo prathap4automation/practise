78104bc03528836af59310611d0eefb0c6394b41
prathap4automation <prathap4automation@gmail.com>
2018-10-01 22:24:19 +0530

    created maven project

---
+++ runner_bat.bat
@@ -0,0 +1,2 @@
+java -cp "E:\selenium\class_programs\lib\*;E:\selenium\class_programs\bin" testngtwo.GoogleTitleTest "GoogleTitleTestByXmlSuite.xml"
+pause
--- src/testngtwo/GoogleTitleTest.java
+++ src/testngtwo/GoogleTitleTest.java
@@ -8,6 +8,7 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -25,7 +26,7 @@ public class GoogleTitleTest {
 	@BeforeMethod
 	public void setup()
 	{
-		System.setProperty("webdriver.chrome.driver", "e:\\selenium\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\chromedriver.exe");
 		driver = new ChromeDriver();
 		wait = new WebDriverWait(driver,20);
 		driver.manage().window().maximize();
@@ -37,19 +38,28 @@ public class GoogleTitleTest {
 	@Parameters({"searchword"})
 	public void search(String x) throws Exception
 	{
-		driver.findElement(By.name("q")).sendKeys(x,Keys.ENTER);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='All']")));
-		String title=driver.getTitle();
-		if(title.contains(x))
-			Assert.assertTrue("Google title test passed", true);
-		else {
-			SimpleDateFormat sf=new SimpleDateFormat("dd-MMM-yy-HH-mm-ss");
-			String ssname=sf.format(new Date())+".png";
-			FileUtils.copyFile(driver.getScreenshotAs(OutputType.FILE), new File("screenshots\\"+ssname));
-			String path="<img src=\""+System.getProperty("user.dir")+"\\screenshots\\"+ssname+"\" alt=\"\" />";
-			Reporter.log(path);
-			Assert.assertTrue("Google title test failed", false);
+		try {
+			driver.findElement(By.name("q")).sendKeys(x,Keys.ENTER);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='All']")));
+			String title=driver.getTitle();
+			if(title.contains(x))
+				Assert.assertTrue("Google title test passed", true);
+			else {
+				SimpleDateFormat sf=new SimpleDateFormat("dd-MMM-yy-HH-mm-ss");
+				String ssname=sf.format(new Date())+".png";
+				FileUtils.copyFile(driver.getScreenshotAs(OutputType.FILE), new File("screenshots\\"+ssname));
+				String path="<img src=\""+System.getProperty("user.dir")+"\\screenshots\\"+ssname+"\" alt=\"\" />";
+				Reporter.log(path);
+				Assert.assertTrue("Google title test failed", false);
+			}
 		}
+		catch(StaleElementReferenceException se) {
+			driver.navigate().refresh();
+		}
+		catch(Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		
 	}
 	
 	@AfterMethod
